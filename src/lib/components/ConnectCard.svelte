70332ece65f5fb0e7c61387c8360b055b26ad197
Connor Tsui <cjtsui@andrew.cmu.edu>
2023-05-28 13:26:33 -0400

    modify connect page

--- src/lib/components/ConnectCard.svelte
+++ src/lib/components/ConnectCard.svelte
@@ -6,19 +6,19 @@
     let cssVarStyles = `--card-color:${social.color}; --card-bg-color:${social.bgColor}`;
 </script>
 
-<div
-    class="flex flex-col overflow-hidden border-[1px] border-t-[#dddddd] border-x-[#cccccc] border-b-[#bbbbbb] rounded-lg"
-    style={cssVarStyles}
->
-    <div class="flex-1 my-card p-16 flex flex-col items-center justify-between">
-        <Fa icon={social.icon} size="2x" />
-        <p class="mt-3">{social.name}</p>
-    </div>
-</div>
-
 <style>
     .my-card {
         background-color: var(--card-bg-color);
         color: var(--card-color);
     }
 </style>
+
+<div
+    class="flex flex-col overflow-hidden rounded-lg"
+    style={cssVarStyles}
+>
+    <div class="flex flex-col flex-1 items-center justify-between my-card p-12 ">
+        <Fa icon={social.icon} size="2x" />
+        <p class="mt-3">{social.name}</p>
+    </div>
+</div>
