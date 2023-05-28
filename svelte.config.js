b8bd8042aaa98a9a6e1ed41ceac70267aeb96805
Connor Tsui <cjtsui@andrew.cmu.edu>
2023-05-28 14:19:07 -0400

    added deploy settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,13 +9,11 @@ const config = {
 
     kit: {
         adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: "build",
-            assets: "build",
-            fallback: undefined,
-            precompress: false,
-            strict: true,
+            // See below for an explanation of these options
+            routes: {
+                include: ["/*"],
+                exclude: ["<all>"],
+            },
         }),
     },
 };
